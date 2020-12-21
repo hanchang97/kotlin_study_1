d176cdc1565db9bec1c53daddacccc2b5b9a24d5
hanchang97 <nimok97@gmail.com>
2020-12-22 04:34:33 +0900

    no message

--- app/src/main/java/com/example/lovetest/MainActivity.kt
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController :NavController //나중에 정의하겠다
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val hi = 0
+        navController = nav_host_fragment.findNavController()
     }
 }
